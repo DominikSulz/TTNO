42a133a7058cc45e5766df817e042f3c24f56043
Dominik Sulz <dominik.sulz@gmail.com>
2023-01-26 16:19:57 +0100

    Initialisation for the TTNO paper
    Initialisation of functions for the TTNO paper

--- Rank_1_check/test_rank_1_TTNO_check.m
+++ Rank_1_check/test_rank_1_TTNO_check.m
@@ -2,22 +2,22 @@
 
 clear all; clc; close all;
 
-addpath('C:\Users\Dominik\Documents\MATLAB\Low rank approximations\rank_adaptive_integrator_for_TTN')
+addpath('C:\Users\Dominik\Documents\MATLAB\Low rank approximations\TTNO')
 
 % physical dimension
-n = 2;
+n = 10;
 tmp = ones(n, 1);
 A = diag(2*tmp,0) - diag(tmp(1:n-1),-1) - diag(tmp(1:n-1),1);
 I = eye(n,n);
 
 % problem
-d = 16;
+d = 8;
 v = 0:(1/(d-1)):1;
 
 % tree structure
-% [X,tau] = init_spin_all_dim_same_rank(4,1,d); % binary tree
+[X,tau] = init_spin_all_dim_same_rank(4,1,d); % binary tree
 % [X,tau] = init_spin_all_dim_diff_rank_TT(d); % tensor train
-[X,tau] = init_spin_all_dim_diff_rank_tree4(d); % tree with 4 subtrees (only for d=16)
+% [X,tau] = init_spin_all_dim_diff_rank_tree4(d); % tree with 4 subtrees (only for d=16)
 
 % linearisation
 B = linearisation_vv(A,d,v);
@@ -25,7 +25,7 @@ B = linearisation_vv(A,d,v);
 % exact operator
 tic
 TTNO_exact = make_operator(X,B,tau,ones(d,1)*n);
-% TTNO_exact = rounding(TTNO_exact,tau);
+TTNO_exact2 = rounding(TTNO_exact,tau);
 time_full_construction = toc
 % TTNO_exact = truncate(TTNO_exact,10^-12,100,2);
 
---
+++ no_structure_interaction/test_no_structure_interaction.m
@@ -0,0 +1,4 @@
+clear all; clc; close all;
+
+addpath('C:\Users\Dominik\Documents\MATLAB\Low rank approximations\TTNO')
+
