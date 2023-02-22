1e188ffb49dbda00729145df5bcc9ad0f69e0eea
Dominik Sulz <dominik.sulz@gmail.com>
2023-02-22 14:42:00 +0100

    HSS unstructured case works

--- HSS_and_no_structure_case/TTNO_HSS_construct_unstructured.m
+++ HSS_and_no_structure_case/TTNO_HSS_construct_unstructured.m
@@ -1,10 +1,10 @@
-function [TTNO] = TTNO_HSS_construct(H,A,l,num_l,n,pos)
-%% for now just copied - has to be adjusted
+function [TTNO] = TTNO_HSS_construct_unstructured(H,A,l,num_l,n,pos)
+
 TTNO = cell(1,4);
 I = eye(n(1),n(1));
 I = I(:);
 
-if l==1 % leaf 
+if l==1 % leaves
     tmp = A{1};
     Ui = [I tmp(:) tmp(:)];
     TTNO{1} = Ui;
@@ -15,12 +15,12 @@ if l==1 % leaf
 else
     len = length(A);
     s = len/2;
-    TTNO{1} = TTNO_HSS_construct(H.A11,A(1:s),l-1,num_l,n(1:s),pos(1:s));
-    TTNO{2} = TTNO_HSS_construct(H.A22,A((s+1):len),l-1,num_l,n((s+1):len),pos((s+1):len));
+    TTNO{1} = TTNO_HSS_construct_unstructured(H.A11,A(1:s),l-1,num_l,n(1:s),pos(1:s));
+    TTNO{2} = TTNO_HSS_construct_unstructured(H.A22,A((s+1):len),l-1,num_l,n((s+1):len),pos((s+1):len));
 end
 % connecting tensor
-m1 = count_leaves(TTNO{1});
-m2 = count_leaves(TTNO{2});
+m1 = count_leaves(TTNO{1}); % number of leaves left
+m2 = count_leaves(TTNO{2}); % number of leaves right
 
 mat_C = zeros((m1+2)*(m2+2),m1+m2+2);
 
@@ -31,19 +31,19 @@ mat_C(1,1) = 1; % identity
 [~,U,~] = full(H.A11);
 [~,~,V] = full(H.A22);
 
-Int = U * H.B12 * V';
+Int = U * H.B12 * V'; % subdiagonal block V(\tau_1,\tau_2)
 
 tmp = zeros(m1+2,m2+2);
 if l==1
     
 else
-    tmp(1,2) = 1;
-    tmp(2,1) = 1;
+    tmp(1,2) = 1; % h_\tau_1
+    tmp(2,1) = 1; % h_\tau_2
 end
 tmp(3:end,3:end) = Int;
 mat_C(:,2) = tmp(:); 
 
-% sinlge interaction
+% single interaction
 E = eye(m1+2,m2+2);
 u1 = E(:,1);
 for ii=1:m1
@@ -58,6 +58,7 @@ for ii=1:m2
     mat_C(:,m1+ii+2) = tmp(:);
 end
 
+% tensorization of the mat_C matrix
 if l==num_l
     mat_C = mat2tens(mat_C(:,2).',[m1+2 m2+2 1],3,1:2);
     TTNO{end-1} = 1;
